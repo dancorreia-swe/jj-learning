acd0ab664fb4267446d00deeb78e1143c6975b0e
Daniel Correia <100008358+dancorreia-swe@users.noreply.github.com>
2025-12-22 15:44:44 -0300

    lowercase function

--- lib/declarative.ex
+++ lib/declarative.ex
@@ -5,4 +5,7 @@ defmodule Declarative do
 
   def upcase_list([]), do: []
   def upcase_list([first | rest]), do: [String.upcase(first) | upcase_list(rest)]
+
+  def lowercase_list([]), do: []
+  def lowercase_list([first | rest]), do: [String.downcase(first) | lowercase_list(rest)]
 end
